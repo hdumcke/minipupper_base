35ca9585a65f1e9a76ba9a628999c0b388c78df4
Horst Dumcke <hdumcke@marlysys.com>
2022-03-13 14:49:22 +0100

    fixed install script for FuelGauge

--- FuelGauge/install.sh
+++ FuelGauge/install.sh
@@ -4,11 +4,16 @@
 
 set -x
 
+### Get directory where this script is installed
+BASEDIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+
 make clean
 make
 make install
 
 sudo cp battery_monitor /usr/bin/
+sudo sed -i "s/BASEDIR/$BASEDIR/" /usr/bin/battery_monitor
 sudo cp battery_monitor.service /lib/systemd/system/
 sudo systemctl enable  battery_monitor.service
 
